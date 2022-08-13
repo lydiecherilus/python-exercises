1c49497620cc250902aa97b3cea7352b11487991
Lydie Cherilus <57009304+lydiecherilus@users.noreply.github.com>
2022-08-12 23:40:48 -0400

    Update swap_char.py

--- swap_char.py
+++ swap_char.py
@@ -2,3 +2,8 @@
 separated by a space and swap the first two characters of each string.
 Sample String : 'abc', 'xyz'
 Expected Result : xyc abz'''
+
+def swap_first_char(str1, str2):
+    temp = str2[0:2] + str1[-1]
+    str2 = str1[0:2] + str2[-1]
+    return temp + ' ' + str2
