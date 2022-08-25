385e1d75aeb92135a0be4b0272add448cc11d6f3
Lydie Cherilus <57009304+lydiecherilus@users.noreply.github.com>
2022-08-25 11:46:04 -0400

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,1 +1,9 @@
 '''Write a Python script to merge two Python dictionaries'''
+
+def merge(dic1, dic2):
+    j = dict()
+    for i in (dic1, dic2):
+        j.update(i)
+    print(j)
+
+merge({1:10, 2:20}, {3:30, 4:40})
