f5e8de8f734bab01948f19cc42a429eb40f2f736
Lydie Cherilus <57009304+lydiecherilus@users.noreply.github.com>
2022-09-05 19:28:57 -0400

    Update number_9s.py

--- number_9s.py
+++ number_9s.py
@@ -1,1 +1,8 @@
 ''' given an array of integers, return the number of 9's in the array'''
+
+def count9(nums):
+  count = 0
+  for i in range(len(nums)):
+    if nums[i] == 9:
+      count = count + 1
+  return count
