15f5ae6b2af328f13284edad8e4ddafe42f50373
Lydie Cherilus <57009304+lydiecherilus@users.noreply.github.com>
2022-09-10 16:15:12 -0400

    Update no_teen.py

--- no_teen.py
+++ no_teen.py
@@ -5,3 +5,9 @@ except 15 and 16 do not count as a teens. Write a separate helper
 In this way, you avoid repeating the teen code 3 times (i.e. "decomposition"). 
 Define the helper below and at the same indent level as the main no_teen_sum().
 '''
+
+def no_teen_sum(a, b, c):
+
+	def fix_teen(n):
+		return n if n not in [13, 14, 17, 18, 19] else 0
+	return fix_teen(a) + fix_teen(b) + fix_teen(c)
